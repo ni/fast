e156fbb30c7e93948f13dd9f14945bcc45a8ae24
Mert Akinc <7282195+m-akinc@users.noreply.github.com>
2026-07-09 17:48:32 -0500

    Refactor tests per request

--- packages/web-components/fast-foundation/src/anchored-region/anchored-region.spec.ts
+++ packages/web-components/fast-foundation/src/anchored-region/anchored-region.spec.ts
@@ -37,8 +37,6 @@ async function setup(parentElement?: HTMLElement) {
 }
 
 describe("Anchored Region", () => {
-    afterEach(() => chai.spy.restore());
-
     it("should set positioning modes to 'uncontrolled' by default", async () => {
         const { element, connect, disconnect } = await setup();
 
@@ -74,138 +72,155 @@ describe("Anchored Region", () => {
         await disconnect();
     });
 
-    interface ScrollListenerTestOptions {
-        createParent: () => HTMLElement,
-        show: (parent: HTMLElement) => void,
-        hide: (parent: HTMLElement) => void,
-        expectListensOnParent: boolean
-    }
+    describe("scroll event listener", () => {
+        let parentAddListenerSpy: any;
+        let parentRemoveListenerSpy: any;
+        let windowAddListenerSpy: any;
+        let windowRemoveListenerSpy: any;
 
-    async function scrollListenerTest({ createParent, show, hide, expectListensOnParent }: ScrollListenerTestOptions) {
-        const parent = createParent();
-        const { element, connect, disconnect } = await setup(parent);
+        function setupSpies(parent: HTMLElement) {
+            parentAddListenerSpy = chai.spy.on(parent, "addEventListener");
+            parentRemoveListenerSpy = chai.spy.on(parent, "removeEventListener");
+            windowAddListenerSpy = chai.spy.on(window, "addEventListener");
+            windowRemoveListenerSpy = chai.spy.on(window, "removeEventListener");
+        }
 
-        // We will re-attach the anchored region after displaying the parent.
-        element.remove();
+        function assertListenedOnTopLayerElement() {
+            expect(windowRemoveListenerSpy).not.to.have.been.called.with("scroll");
+            expect(windowAddListenerSpy).not.to.have.been.called.with("scroll");
+            expect(parentAddListenerSpy).to.have.been.called.with("scroll");
+            expect(parentRemoveListenerSpy).to.have.been.called.with("scroll");
+        }
 
-        const parentAddListenerSpy = chai.spy.on(parent, "addEventListener");
-        const parentRemoveListenerSpy = chai.spy.on(parent, "removeEventListener");
-        const windowAddListenerSpy = chai.spy.on(window, "addEventListener");
-        const windowRemoveListenerSpy = chai.spy.on(window, "removeEventListener");
+        function assertListenedOnWindow() {
+            expect(parentRemoveListenerSpy).not.to.have.been.called.with("scroll");
+            expect(parentAddListenerSpy).not.to.have.been.called.with("scroll");
+            expect(windowAddListenerSpy).to.have.been.called.with("scroll");
+            expect(windowRemoveListenerSpy).to.have.been.called.with("scroll");
+        }
 
-        await connect();
-        show(parent);
-        parent.appendChild(element);
-        hide(parent);
-        await disconnect();
+        function assertChangedToListeningOnWindow() {
+            expect(windowRemoveListenerSpy).not.to.have.been.called.with("scroll");
+            expect(parentAddListenerSpy).not.to.have.been.called.with("scroll");
+            expect(parentRemoveListenerSpy).to.have.been.called.with("scroll");
+            expect(windowAddListenerSpy).to.have.been.called.with("scroll");
+        }
 
-        const expectedAddSpy = expectListensOnParent ? parentAddListenerSpy : windowAddListenerSpy;
-        const expectedRemoveSpy = expectListensOnParent ? parentRemoveListenerSpy : windowRemoveListenerSpy;
-        const notExpectedAddSpy = expectListensOnParent ? windowAddListenerSpy : parentAddListenerSpy;
-        const notExpectedRemoveSpy = expectListensOnParent ? windowRemoveListenerSpy : parentRemoveListenerSpy;
+        function assertChangedToListeningOnTopLayerElement() {
+            expect(parentRemoveListenerSpy).not.to.have.been.called.with("scroll");
+            expect(windowAddListenerSpy).not.to.have.been.called.with("scroll");
+            expect(windowRemoveListenerSpy).to.have.been.called.with("scroll");
+            expect(parentAddListenerSpy).to.have.been.called.with("scroll");
+        }
 
-        expect(notExpectedRemoveSpy).not.to.have.been.called.with("scroll");
-        expect(notExpectedAddSpy).not.to.have.been.called.with("scroll");
-        expect(expectedAddSpy).to.have.been.called.with("scroll");
-        expect(expectedRemoveSpy).to.have.been.called.with("scroll");
-    }
+        async function setupScrollListenerTest(parent: HTMLElement) {
+            const { element, connect, disconnect } = await setup(parent);
 
-    it("should attach/detach scroll listener to ancestor modal dialog instead of window", async () => {
-        await scrollListenerTest({
-            createParent: () => document.createElement("dialog"),
-            show: (dialog: HTMLDialogElement) => dialog.showModal(),
-            hide: (dialog: HTMLDialogElement) => dialog.close(),
-            expectListensOnParent: true
+            // We will re-attach the anchored region after displaying the parent.
+            element.remove();
+            setupSpies(parent);
+            await connect();
+
+            return { region: element, disconnect };
+        }
+
+        afterEach(() => chai.spy.restore());
+
+        it("should be on containing modal dialog instead of window", async () => {
+            const dialog = document.createElement("dialog");
+            const { region, disconnect } = await setupScrollListenerTest(dialog);
+            dialog.showModal();
+
+            dialog.appendChild(region); // adds scroll listener
+            region.remove(); // removes scroll listener
+
+            dialog.close();
+            await disconnect();
+
+            assertListenedOnTopLayerElement();
         });
-    });
 
-    it("should attach scroll listener to window when ancestor dialog is not modal", async () => {
-        await scrollListenerTest({
-            createParent: () => document.createElement("dialog"),
-            show: (dialog: HTMLDialogElement) => dialog.show(),
-            hide: (dialog: HTMLDialogElement) => dialog.close(),
-            expectListensOnParent: false
+        it("should be on window when in non-modal dialog", async () => {
+            const dialog = document.createElement("dialog");
+            const { region, disconnect } = await setupScrollListenerTest(dialog);
+            dialog.show();
+
+            dialog.appendChild(region); // adds scroll listener
+            region.remove(); // removes scroll listener
+
+            dialog.close();
+            await disconnect();
+
+            assertListenedOnWindow();
         });
-    });
 
-    it("should attach/detach scroll listener to ancestor open popover instead of window", async () => {
-        await scrollListenerTest({
-            createParent: () => {
-                const div = document.createElement("div");
-                div.setAttribute("popover", "");
-                return div;
-            },
-            show: (element) => element.showPopover(),
-            hide: (element) => element.hidePopover(),
-            expectListensOnParent: true
+        it("should be on containing popover element instead of window", async () => {
+            const popoverDiv = document.createElement("div");
+            popoverDiv.setAttribute("popover", "");
+            const { region, disconnect } = await setupScrollListenerTest(popoverDiv);
+            popoverDiv.showPopover();
+
+            popoverDiv.appendChild(region); // adds scroll listener
+            region.remove(); // removes scroll listener
+
+            popoverDiv.hidePopover();
+            await disconnect();
+
+            assertListenedOnTopLayerElement();
         });
-    });
 
-    it("should attach/detach scroll listener to ancestor fullscreen element instead of window", async () => {
-        await scrollListenerTest({
-            createParent: () => document.createElement("div"),
-            show: (element) => {
-                Object.defineProperty(document, "fullscreenElement", {
-                    get: () => element,
-                    configurable: true,
-                });
-            },
-            hide: () => {
-                Object.defineProperty(document, "fullscreenElement", {
-                    get: () => null,
-                    configurable: true,
-                });
-            },
-            expectListensOnParent: true
+        it("should be on containing fullscreen element instead of window", async () => {
+            const fullscreenDiv = document.createElement("div");
+            const { region, disconnect } = await setupScrollListenerTest(fullscreenDiv);
+            Object.defineProperty(document, "fullscreenElement", {
+                get: () => fullscreenDiv,
+                configurable: true,
+            });
+
+            fullscreenDiv.appendChild(region); // adds scroll listener
+            region.remove(); // removes scroll listener
+
+            Object.defineProperty(document, "fullscreenElement", {
+                get: () => null,
+                configurable: true,
+            });
+            await disconnect();
+
+            assertListenedOnTopLayerElement();
         });
-    });
 
-    async function topLayerTransitionTest(startInTopLayer: boolean) {
-        const dialog = document.createElement("dialog");
-        const { element, connect, disconnect } = await setup(dialog);
+        it("should switch to containing dialog when it is shown as modal", async () => {
+            const dialog = document.createElement("dialog");
+            const { connect, disconnect } = await setup(dialog);
+            await connect();
+            setupSpies(dialog);
 
-        if (startInTopLayer) {
+            // Move into top layer
+            dialog.showModal();
+            await DOM.nextUpdate();
+
+            assertChangedToListeningOnTopLayerElement();
+
+            dialog.close();
+            await disconnect();
+        });
+
+        it("should switch to window when containing dialog stops being modal", async () => {
+            const dialog = document.createElement("dialog");
+            const { element, connect, disconnect } = await setup(dialog);
             element.remove();
             await connect();
             dialog.showModal();
             dialog.appendChild(element);
-        } else {
-            await connect();
-        }
+            setupSpies(dialog);
 
-        const dialogAddListenerSpy = chai.spy.on(dialog, "addEventListener");
-        const dialogRemoveListenerSpy = chai.spy.on(dialog, "removeEventListener");
-        const windowAddListenerSpy = chai.spy.on(window, "addEventListener");
-        const windowRemoveListenerSpy = chai.spy.on(window, "removeEventListener");
-
-        if (startInTopLayer) {
             // Move out of top layer
             dialog.close();
-        } else {
-            // Move into top layer
-            dialog.showModal();
-        }
-        await DOM.nextUpdate();
+            await DOM.nextUpdate();
 
-        const expectedRemoveSpy = startInTopLayer ? dialogRemoveListenerSpy : windowRemoveListenerSpy;
-        const expectedAddSpy = startInTopLayer ? windowAddListenerSpy : dialogAddListenerSpy;
-        const notExpectedAddSpy = startInTopLayer ? dialogAddListenerSpy : windowAddListenerSpy;
-        const notExpectedRemoveSpy = startInTopLayer ? windowRemoveListenerSpy : dialogRemoveListenerSpy;
+            assertChangedToListeningOnWindow();
 
-        expect(notExpectedRemoveSpy).not.to.have.been.called.with("scroll");
-        expect(notExpectedAddSpy).not.to.have.been.called.with("scroll");
-        expect(expectedRemoveSpy).to.have.been.called.with("scroll");
-        expect(expectedAddSpy).to.have.been.called.with("scroll");
-
-        dialog.close();
-        await disconnect();
-    }
-
-    it("should move scroll listener to dialog when it enters the top layer", async () => {
-        await topLayerTransitionTest(false);
-    });
-
-    it("should move scroll listener to window when dialog leaves the top layer", async () => {
-        await topLayerTransitionTest(true);
+            await disconnect();
+        });
     });
 });
